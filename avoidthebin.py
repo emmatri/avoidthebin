429e191d7dc31184000ec4fc8dd5424aec4dc72a
jessicaamy@gmail.com <jessicaamy@gmail.com>
2019-03-20 17:51:44 +0000

    able to add and view items

--- avoidthebin.py
+++ avoidthebin.py
@@ -10,41 +10,32 @@ def home():
 def listit():
 	return render_template("listit.html")
 
-userarray = []
-
-people = [{'food': u'Onion', 'name': 'Jess', 'postcode': u'EC1N 2TD'}, {'food': u'Banana', 'name': u'Ben', 'postcode': u'S12d'}, {'food': u'Cheese', 'name': 'Jess', 'postcode': u'EC1N 3TD'}, {'food': u'Onion', 'name': 'Tom', 'postcode': u'EP18 0FR'}]
+users = []
 
 def getuniquefood():
 	i = 0
 	x = []
-	while i < len(people):
-		if people[i]["food"] not in x:
-			x.append(people[i]["food"])
+	print len(users)
+	while i < len(users):
+		if users[i]["food"] not in x:
+			x.append(users[i]["food"])
 		i += 1
 	return x
 
-listfood = getuniquefood()
-print listfood
-
 @app.route("/listed")
 def listed():
-	return render_template('listed.html', listed = listfood)
+	return render_template('listed.html', listed = getuniquefood())
 
 
-# @app.route("/listedtwo", methods=["POST"])
-# def list_it():
-# 	form_data = request.form
-# 	name = form_data["name"]
-# 	food = form_data["food"]
-# 	postcode = form_data["postcode"]
+@app.route("/added", methods=["POST"])
+def list_it():
+	form_data = request.form
+	name = form_data["name"]
+	food = form_data["food"]
+	postcode = form_data["postcode"]
 
-# 	userarray.append([{"name": name, "food": food,"postcode": postcode}])
-# 	print postcode
-# 	print name
-# 	print food
-# 	print userarray
-# 	uniquefood = []
-# 	return "Listed OK" + food
+	users.append({"name": name, "food": food,"postcode": postcode})
+	return "Thanks " + name + "! You have successfully added " + food
 
 
 app.run(debug=True)
